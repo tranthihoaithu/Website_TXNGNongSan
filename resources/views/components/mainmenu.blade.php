6e41ef6edcbe85ce92dd884b4b1993e849052e55
trantienthe <tranthe2k2ak@gmail.com>
2023-12-23 01:11:26 +0700

    add components giao dien website

---
+++ resources/views/components/mainmenu.blade.php
@@ -0,0 +1,6 @@
+						<div class="mainmenu pull-left">
+							<ul class="nav navbar-nav collapse navbar-collapse">
+								<li><a href="{{ route('home') }}" class="active">Đại lý</a></li>
+								<li><a href="{{ route('nhabanle') }}">Nhà bán lẻ</a></li>						
+							</ul>
+						</div>
